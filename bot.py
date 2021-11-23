ca7ebce83793f91a3317ad0be18ad18a327a9a61
vivekgandhari <45706156+vivekgandhari@users.noreply.github.com>
2021-11-23 20:03:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,8 +4,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello world!"
+def start():
+    querry = ""
+    while(querry!="exit"):
+        querry = input("Please input your question: ")
+        print("The code is yet to be updated to answer your question: "+querry"+"\n"+"Thank you")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
