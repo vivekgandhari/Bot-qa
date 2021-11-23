fc137b406317b0b68885e70f407b5bb151da61ec
vivekgandhari <45706156+vivekgandhari@users.noreply.github.com>
2021-11-23 18:51:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,11 @@
+
+BUILD_DIR=$1
+
+# Exit early if app is clearly not Python.
+if [ ! -f "$BUILD_DIR/requirements.txt" ] && [ ! -f "$BUILD_DIR/setup.py" ] && [ ! -f "$BUILD_DIR/Pipfile" ]; then
+  exit 1
+fi
+
 s = input("Type querry: ")
 print(s)
 print("The answering is being processed")
