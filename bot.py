cc1ae1be7815ac8ec6f2ff5e73dc2a20d28d0e31
vivekgandhari <45706156+vivekgandhari@users.noreply.github.com>
2021-11-23 19:52:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
-while(1):
-  s = input("querry: ")
-  print("Bot deployment successful for querry: " + s)
+import os
+from intro_to_flask import app
+
+port = int(os.environ.get("PORT", 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
