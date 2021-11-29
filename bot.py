2c93e2dcb71696e24c745464fc52b77dbdca4775
vivekgandhari <45706156+vivekgandhari@users.noreply.github.com>
2021-11-29 16:14:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,34 @@
-import os
-from flask import Flask,requests
+# import os
+# from flask import Flask,requests
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/form')
-def form():
-    return render_template('form.html')
+# @app.route('/form')
+# def form():
+#     return render_template('form.html')
  
-@app.route('/data/', methods = ['POST', 'GET'])
-def data():
-    if request.method == 'GET':
-        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-    if request.method == 'POST':
-        form_data = request.form
-        return render_template('data.html',form_data = form_data)
+# @app.route('/data/', methods = ['POST', 'GET'])
+# def data():
+#     if request.method == 'GET':
+#         return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+#     if request.method == 'POST':
+#         form_data = request.form
+#         return render_template('data.html',form_data = form_data)
  
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host='0.0.0.0', port=port)
+from pywebio.input import input
+from pywebio.output import put_text
+
+def bmi():
+    height = input("Your height:")
+    weight = input("Your weight:")
+    
+    BMI_cal = height/weight
+    
+    put_text(BMI_cal)
+
+
+if __name__ == '__main__':
+    bmi()
