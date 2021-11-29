a94ee90ab640375659f3f7baba4a00afec935dee
vivekgandhari <45706156+vivekgandhari@users.noreply.github.com>
2021-11-29 17:24:50 +0530

    Update ml_demo_code.py

--- ml_demo_code.py
+++ ml_demo_code.py
@@ -10,19 +10,19 @@ from extract_answer import *
 
 warnings.filterwarnings("ignore")
 
-with open('./pickledFiles/random_forest_model.pkl', 'rb') as f:
-   random_forest_model = pickle.load(f)
+# with open('./pickledFiles/random_forest_model.pkl', 'rb') as f:
+#    random_forest_model = pickle.load(f)
 
-with open('./pickledFiles/columns.pkl', 'rb') as f:
-   model_columns = pickle.load(f)
+# with open('./pickledFiles/columns.pkl', 'rb') as f:
+#    model_columns = pickle.load(f)
 
-def prediction(prediction_df):
-    query_ = pd.get_dummies(pd.DataFrame(prediction_df, index = [0]), prefix=['Sector','job_sim'], columns=['Sector','job_sim'])
-    query = query_.reindex(columns = model_columns, fill_value= 0)
-    result = list(random_forest_model.predict(query))
-    final_result = round(result[0], 3)
+# def prediction(prediction_df):
+#     query_ = pd.get_dummies(pd.DataFrame(prediction_df, index = [0]), prefix=['Sector','job_sim'], columns=['Sector','job_sim'])
+#     query = query_.reindex(columns = model_columns, fill_value= 0)
+#     result = list(random_forest_model.predict(query))
+#     final_result = round(result[0], 3)
 
-    return final_result
+#     return final_result
 
 def main():
     put_markdown(
@@ -33,31 +33,12 @@ def main():
     )
     querry = input("Question:")
     put_text(extract_answer(querry))
-    put_button("Try another question",onclick=main)
+    put_button("Try another question",onclick=next_question)
     
-    
-#     model_inputs = input_group(
-#         "Enter the following information",
-#         [
-#             input("Rating of the Job", name='rating', type=FLOAT),
-#             select("Job Sector", name='job_sec', options=[(i,i) for i in ['Information Technology', 'Business Services', 'Education', 'Finance', 'Government', 'Travel & Tourism', 'Health Care']]),
-#             select("Job Role", name='job_role', options=[('Data Scientist', 'data scientist'), ('Data Engineer', 'data engineer'), ('Analyst', 'analyst'), ('Machine Learning Engineer', 'mle'), ('Director', 'director'), ('Manager', 'manager')]),
-#             radio("Are you familiar with Python?", name='py_choice', options=[('Yes', 1), ('No', 0)]),
-#             radio("Are you familiar with R?", name='r_choice', options=[('Yes', 1), ('No', 0)]),
-#             radio("Are you familiar with Tableau?", name='t_choice', options=[('Yes', 1), ('No', 0)]),
-#             radio("Are you familiar with Power Bi?", name='pi_choice', options=[('Yes', 1), ('No', 0)]),
-#             radio("Are you familiar with Machine Learning?", name='ml_choice', options=[('Yes', 1), ('No', 0)]),
-#             radio("Are you familiar with Deep Learning?", name='dl_choice', options=[('Yes', 1), ('No', 0)]),
-#         ]
-#     )
-
-#     prediction_df = pd.DataFrame(data = [[model_inputs[i] for i in ['job_sec', 'job_role', 'py_choice', 'r_choice', 't_choice','pi_choice','ml_choice', 'dl_choice', 'rating']]], 
-#                            columns = ['Sector','job_sim','python_yn','R_yn','tableau','power bi','ml','dl', 'Rating'])
-
-#     expectedSalary = prediction(prediction_df)
-#     put_markdown("### Predicted Salary: {}k Dollars".format(expectedSalary))
-
-
+def next_question():
+   querry = input("Question:")
+   put_text(extract_answer(querry))
+   put_button("Try another question",onclick=main)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
